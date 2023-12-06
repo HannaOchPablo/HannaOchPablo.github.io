4a4ca90c601d3a98ad1b5da128d0936e7f5433af
HannaOchPablo <153135703+HannaOchPablo@users.noreply.github.com>
2023-12-06 23:46:57 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+// Define the Guest object constructor
+function Guest(name, canBringPlusOne) {
+  this.name = name;
+  this.canBringPlusOne = canBringPlusOne;
+}
+
+// Example usage:
+const guest1 = new Guest("Hanna Pettersson Nyman", true);
+const guest2 = new Guest("Pablo Ahava Pizarro", false);
+
+  
+  // Dummy guest list for testing
+  const guestList = [guest1, guest2];
